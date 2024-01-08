00a2f7f18be047441cb423615b835fa8ede9835e
Christopher Siu <christopher@siu.sh>
2024-01-08 15:52:01 -0800

    Added hello world example.

--- 01.08-make/hello.c
+++ 01.08-make/hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    printf("Hello, world!\n");
+/* NOTE: The arguments to main represent the command line arguments to the
+ *       program; they can be declared as void if they are unused. */
+int main(void) {
+    printf("Goodbye, world!\n");
 
     return 0;
 }
