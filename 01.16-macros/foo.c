415b1afbf87384cd5d09e803868036c0dab758bb
Christopher Siu <christopher@siu.sh>
2024-01-16 11:17:36 -0800

    Added base code for macros.

---
+++ 01.16-macros/foo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Recall the following function: */
+int add(int x, int y) {
+    return x + y;
+}
+
+int main(void) {
+    printf("add(1, 2): %d\n", add(1, 2));
+    
+    return 0;
+}
