316879715e0c8b00c75b655fcdc18446e53d1aab
Christopher Siu <christopher@siu.sh>
2024-01-19 13:34:10 -0800

    Added example of arrays as arguments.

--- 01.19-arrays/main.c
+++ 01.19-arrays/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include "rev.h"
 
 #define LENGTH 4
 
 int main(void) {
+    /* To declare an array of four integers, where the first is 1 and the
+     *  second is 2 (the compiler will zero out the rest): */
     int arr[LENGTH] = {1, 2};
 
+    rev(arr, LENGTH);
     printf("arr: %p\n", (void *)arr);
     printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
     printf(" |- %p: %d\n", (void *)&arr[1], arr[1]);
--- 01.19-arrays/rev.c
+++ 01.19-arrays/rev.c
@@ -1,5 +1,15 @@
 #include "rev.h"
 
+/* To define a function that takes as argument an array of integers -- note
+ *  that the length must be passed separately: */
 void rev(int arr[], int len) {
+    int i;
 
+    /* Since an array is an address, its value is a reference, and this will
+     *  modify the caller's copy: */
+    for (i = 0; i < len / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[len - 1 - i];
+        arr[len - 1 - i] = temp;
+    }
 }
