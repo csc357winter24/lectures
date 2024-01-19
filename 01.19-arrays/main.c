57251a0007b0a11e6a0c2742c9154b36c6693229
Christopher Siu <christopher@siu.sh>
2024-01-19 08:06:17 -0800

    Added base examples of arrays.

---
+++ 01.19-arrays/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define LENGTH 4
+
+int main(void) {
+    int arr[LENGTH] = {1, 2};
+
+    printf("arr: %p\n", (void *)arr);
+    printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
+    printf(" |- %p: %d\n", (void *)&arr[1], arr[1]);
+    printf(" |- %p: %d\n", (void *)&arr[2], arr[2]);
+    printf(" +- %p: %d\n", (void *)&arr[3], arr[3]);
+
+    return 0;
+}
---
+++ 01.19-arrays/rev.c
@@ -0,0 +1,5 @@
+#include "rev.h"
+
+void rev(int arr[], int len) {
+
+}
