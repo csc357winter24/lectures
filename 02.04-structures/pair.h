d6bdc2fd333322d99ecb16714e6ab1560671bb7c
Christopher Siu <christopher@siu.sh>
2024-02-05 18:09:43 -0800

    Added example of structures.

--- 02.04-structures/pair.h
+++ 02.04-structures/pair.h
@@ -3,3 +3,7 @@ struct Pair {
     int first;
     int second;
 };
+
+/* The type defined above is "struct Pair", not "Pair"; to define "Pair", we
+ *  can define it as an alternative name for "struct Pair": */
+typedef struct Pair Pair;
--- 02.04-structures/trip.h
+++ 02.04-structures/trip.h
@@ -1,3 +1,4 @@
+/* Anyone who includes trip.h will also get a copy of pair.h: */
 #include "pair.h"
 
 /* Trip: Represents a triple of integers. */
