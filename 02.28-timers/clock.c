e890e25603df771646b8598ebda4272f57fd53db
cesiu <cesiu@calpoly.edu>
2024-02-29 19:38:11 -0800

    Added examples of timers and sending signals.

--- 02.28-timers/clock.c
+++ 02.28-timers/clock.c
@@ -5,18 +5,37 @@
 #include <signal.h>
 
 void handler(int signum) {
-    printf("Handling signal %d.\n", signum);
+    /* This handler doesn't actually have to do anything, we just need to
+     *  change the default response to SIGALRM, which is to terminate. */
 }
 
 int main(void) {
     struct sigaction action;
+    struct itimerval timer;
+    int i = 0;
 
     action.sa_handler = handler;
     action.sa_flags = SA_RESTART;
     sigemptyset(&action.sa_mask);
     sigaction(SIGALRM, &action, NULL);
 
+    /* Initialize a timer for one second: */
+    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_usec = 0;
+
+    /* Every time the timer elapses, reset it to one second: */
+    timer.it_interval = timer.it_value;
+
+    /* Start the timer: */
+    setitimer(ITIMER_REAL, &timer, NULL);
+
     while(1) {
+        /* Rather than busy-waiting, we can just halt execution until a signal
+         *  is sent -- and now that we're effectively responding to the signal
+         *  in the main program, we have access to all of the main program's
+         *  information: */
+        pause();
+        printf("%d\n", ++i);
     }
 
     return EXIT_SUCCESS;
--- 02.28-timers/spork.c
+++ 02.28-timers/spork.c
@@ -3,18 +3,46 @@
 #include <unistd.h>
 #include <signal.h>
 
-/* NOTE: For simplicity, we assume that only one
- *       child will be sporked at a time. */
+/* NOTE: For simplicity, we assume that only one child will be sporked at a
+ *       time. This needs to be global so that it is accessible to the
+ *       handler.*/
 static pid_t child = 0;
 
+/* NOTE: This is technically not quite safe; there is theoretically a very
+ *       small chance that the sporked child exits before the timer, and
+ *       during that window of time, its PID is reused for a different process,
+ *       which would then be killed. */
 static void handler(int signum) {
+    kill(child, SIGKILL);
 }
 
 /* spork: Creates a new resource-limited process. */
 pid_t spork(time_t timeout) {
+    struct rlimit limit;
+    struct sigaction action;
+    struct itimerval timer;
+
     if ((child = fork()) == 0) {
+        /* Lower to the child's soft and hard limits on number of processes: */
+        limit.rlim_cur = 1;
+        limit.rlim_max = 1;
+        setrlimit(RLIMIT_NPROC, &limit);
     }
     else {
+        /* Install a signal handler for SIGALRM -- this has to happen before
+         *  the timer is started, otherwise there is a chance that the timer
+         *  could go off before we know how to respond. */
+        action.sa_handler = handler;
+        action.sa_flags = SA_RESTART;
+        sigemptyset(&action.sa_mask);
+        sigaction(SIGALRM, &action, NULL);
+
+        /* Set a timer for the given number of seconds: */
+        timer.it_value.tv_sec = timeout;
+        timer.it_value.tv_usec = 0;
+        timer.it_interval.tv_sec = 0;
+        timer.it_interval.tv_usec = 0;
+        setitimer(ITIMER_REAL, &timer, NULL);
     }
 
     return child;
