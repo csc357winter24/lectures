e5178d14a7ba2ae1a37d8ebe645438ac102a858e
cesiu <cesiu@calpoly.edu>
2024-03-06 15:54:27 -0800

    Added example of multiple pipes.

--- 03.06-deadlocks/hello.c
+++ 03.06-deadlocks/hello.c
@@ -4,6 +4,11 @@
 int main(void) {
     char buf[16];
 
+    /* This program has no idea that pipes might be involved -- it simply
+     *  expects to use stdin and stdout. We will have to replace stdin and/or
+     *  stdout with pipes in order to "trick" this program into using the
+     *  pipes instead... */
+
     while (fgets(buf, 16, stdin) != NULL) {
         buf[strlen(buf) - 1] = '\0';
         printf("Hello, %s!\n", buf);
