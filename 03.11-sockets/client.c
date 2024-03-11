fb0de96b757f8275aca8060c4707d6d2a84362ab
cesiu <cesiu@calpoly.edu>
2024-03-11 15:58:32 -0700

    Added example of sockets.

--- 03.11-sockets/client.c
+++ 03.11-sockets/client.c
@@ -6,6 +6,8 @@
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
+    int fd, n = 0;
+    char buf[15] = "Hello, server!";
 
     /* Use TCP/IPv4: */
     hints.ai_family = AF_INET;
@@ -14,6 +16,23 @@ int main(int argc, char *argv[]) {
     /* Use a specified address and port: */
     getaddrinfo(argv[1], argv[2], &hints, &addr);
 
+    /* Use the first address information produced by getaddrinfo to create
+     *  a socket -- we'll assume the first address works. The newly created
+     *  socket takes the form of a file descriptor, so we can interact with it
+     *  just like any other form of I/O: */
+    fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    /* Connect that socket to the server: */
+    connect(fd, addr->ai_addr, addr->ai_addrlen);
+
+    /* It is possible that not all of the data could be sent at once, in which
+     *  case it is our responsibility to make sure we send the rest of the data
+     *  later: */
+    while (n < 14) {
+        n += write(fd, buf + n, 14 - n); 
+    }
+
+    close(fd);
     freeaddrinfo(addr);
 
     return EXIT_SUCCESS;
--- 03.11-sockets/server.c
+++ 03.11-sockets/server.c
@@ -6,15 +6,46 @@
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
+    int fd, client, n;
+    char buf[5];
 
     /* Use TCP/IPv4: */
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
 
-    /* Use our own address and a specified port: */
+    /* Use our own address and a specified port. "AI_PASSIVE" indicates that
+     *  we don't plan to actively create any new connections; we just want to
+     *  passively wait for other processes to connect to us. */
     hints.ai_flags = AI_PASSIVE;
     getaddrinfo(NULL, argv[1], &hints, &addr);
 
+    /* Use that information to create a socket and bind it to a specific port;
+     *  clients will need to know this port to establish connections. */
+    fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    bind(fd, addr->ai_addr, addr->ai_addrlen);
+
+    /* Listen for new connections on the bound port; for simplicity, we'll
+     *  assume that only one client will ever attempt to connect. If we had to
+     *  handle multiple clients, we could request that they be queued up. */
+    listen(fd, 1);
+
+    /* Accept the next new connection. If we wanted to know whose connection we
+     *  just accepted, we could pass additional pointers to structures which
+     *  will be populated with information about the client, for example, if we
+     *  only wanted to talk to clients with certain IP addresses. */
+    client = accept(fd, NULL, NULL);
+
+    /* Note that accept creates a new socket and returns a new file descriptor
+     *  for use when communicating with the accepted client. The existing file
+     *  descriptor and its bound port are left untouched, still listening for
+     *  potential future connections. */
+    while ((n = read(client, buf, 4)) > 0) {
+        buf[n] = '\0';
+        printf("Read \"%s\" from client.\n", buf);
+    }
+
+    close(client);
+    close(fd);
     freeaddrinfo(addr);
 
     return EXIT_SUCCESS;
