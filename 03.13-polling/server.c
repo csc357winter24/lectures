4d074b23b700b90e1d2fe87a0a97cd8242292739
cesiu <cesiu@calpoly.edu>
2024-03-13 12:58:45 -0700

    Added example of polling non-blocking descriptors.

--- 03.13-polling/server.c
+++ 03.13-polling/server.c
@@ -1,13 +1,15 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
     struct addrinfo hints = {0}, *addr;
-    int fd, client, n;
-    char buf[5];
+    struct pollfd clients[17];
+    int fd, n, i;
 
     /* Use TCP/IPv4, our own address, and a specified port: */
     hints.ai_family = AF_INET;
@@ -20,15 +22,49 @@ int main(int argc, char *argv[]) {
     bind(fd, addr->ai_addr, addr->ai_addrlen);
     listen(fd, 1);
 
-    /* Accept the next new connection: */
-    client = accept(fd, NULL, NULL);
+    /* Add the listening socket to the array of polled descriptors: */
+    clients[0].fd = fd;
+    clients[0].events = POLLIN;
+    n = 1;
 
-    /* Copy from the socket to stdout: */
-    while ((n = read(client, buf, 4)) > 0) {
-        write(STDOUT_FILENO, buf, n);
+    /* Run until no clients connect for 10 seconds: */
+    while (poll(clients, n, n > 1 ? -1 : 10000) > 0) {
+        /* Note that poll returns the *number* of descriptors that have data
+         *  to be read, not *which* descriptor. */
+        for (i = 0; i < n; i++) {
+            if (clients[i].revents & POLLIN) {
+                if (clients[i].fd == fd) {
+                    /* Accept the next new connection and add it to the array
+                     *  of descriptors to be polled: */
+                    clients[n].fd = accept(fd, NULL, NULL);
+                    clients[n].events = POLLIN;
+
+                    /* Set that connection to non-blocking: */
+                    fcntl(clients[n].fd, F_SETFL, O_NONBLOCK);
+                    n++;
+                }
+                else {
+                    /* Copy from the socket to stdout: */
+                    char buf[5];
+                    int m;
+
+                    while ((m = read(clients[i].fd, buf, 4)) > 0) {
+                        write(STDOUT_FILENO, buf, m);
+                    }
+
+                    /* With the connections made non-blocking, we can
+                     *  distinguish between "no data for now" and "no data
+                     *  will ever come in the future": */
+                    if (m == 0) {
+                        close(clients[i].fd);
+                        clients[i] = clients[n - 1];
+                        n--;
+                    }
+                }
+            }
+        }
     }
 
-    close(client);
     close(fd);
     freeaddrinfo(addr);
 
